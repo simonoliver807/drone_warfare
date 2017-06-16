b0cca2fc838bb7483ee46e65c29b5bb28009013f
simonoliver807 <simon.oliver807@gmail.com>
2017-06-16 18:01:10 +0100

    fixes 1606

--- game/js/main.js
+++ game/js/main.js
@@ -91,17 +91,17 @@ define(['gameinit','v3d'], function(GAMEINIT,V3D){
 			handleKeyDown: function( event ) {
 
 				// change to live
-			    //event.preventDefault();
+			    event.preventDefault();
 			    // change to live
-				if( event.keyCode === 80) {
-					var val = gameinit.gspause() ? 0: 1;
-				    gameinit.gspause(val);
-				}
+				// if( event.keyCode === 80) {
+				// 	var val = gameinit.gspause() ? 0: 1;
+				//     gameinit.gspause(val);
+				// }
 				// change to live
-				if ( event.keyCode == 83) {
-					var bodys = gameinit.getObj('bodys');
-					bodys[0].body.linearVelocity.init();
-				}
+				// if ( event.keyCode == 83) {
+				// 	var bodys = gameinit.getObj('bodys');
+				// 	bodys[0].body.linearVelocity.init();
+				// }
 				if ( event.keyCode == 70) {
 					document.body.webkitRequestFullScreen();
 				}
